550ba1678292a722017ad2b8f1d4f095fded6cc4
Sebastian Flor <sebastian.flor@atheneum.ai>
2024-09-11 16:45:48 +0200

    feat: create page custom heder

--- app/(tabs)/explore.tsx
+++ app/(tabs)/explore.tsx
@@ -1,12 +1,27 @@
-import { Link } from "expo-router";
+import { ExploreHeader } from "@/components/ExploreHeader";
+import { Listings } from "@/components/Listings";
+import { Link, Stack } from "expo-router";
+import { useMemo, useState } from "react";
 import { View, Text } from "react-native";
 
+import listingsData from "@/assets/data/airbnb-listings.json";
+
 export default function Page() {
+  const [category, setCategory] = useState("Tiny homes");
+  const items = useMemo(() => listingsData as any, []);
+
+  const onDataChanged = (_category: string) => {
+    setCategory(_category);
+  };
+
   return (
-    <View>
-      <Link href={"/(modals)/login"}>Login</Link>
-      <Link href={"/(modals)/booking"}>Booking</Link>
-      <Link href={"/listing/1"}>listing details</Link>
+    <View style={{ flex: 1, marginTop: 130 }}>
+      <Stack.Screen
+        options={{
+          header: () => <ExploreHeader onCategoryChanged={onDataChanged} />,
+        }}
+      />
+      <Listings listings={items} category={category} refresh={1} />
     </View>
   );
 }
